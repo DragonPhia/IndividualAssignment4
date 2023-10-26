22ad31074e4509523b8722e18949a7f06e54e31b
Dragon Phiansin <142546629+DragonPhia@users.noreply.github.com>
2023-10-25 21:48:08 -0400

    pushed files into git

---
+++ CSC340Assignment4/src/main/java/com/mycompany/csc340assignment4/CSC340Assignment4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.csc340assignment4;
+
+/**
+ *
+ * @author dragon
+ */
+public class CSC340Assignment4 {
+
+    public static void main(String[] args) {
+        System.out.println("It's Working! Please run the Data.java file!");
+    }
+}
